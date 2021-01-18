41dd86e5ebbce68ba449bcd430e30a41aa00938a
Thomas Hollevoet <thomas.hollevoet@student.odisee.be>
2021-01-18 18:08:18 +0100

    see if handleCamera can be async

--- src/gameview.ts
+++ src/gameview.ts
@@ -112,7 +112,7 @@ export class GameView {
         });             
     }
 
-    private handleCameraMovement() {
+    private async handleCameraMovement() {
         // Set raycaster according to the XR camera
         let xr = this.renderer.xr.getCamera(this.camera);
         this.workingMatrix.identity().extractRotation( xr.matrixWorld );
